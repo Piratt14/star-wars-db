3158e6e7e0e5570c0b4a75580d4f0fa1b916b1f2
Piratt14 <satdvlad@gmail.com>
2019-12-09 01:03:33 +0300

    many updates ago, but now main component can change his service from real to dummy and contrariwise; use SwapiServiceProvider from react Context for transmission data service to any element; getting customized items from sw-components; swapiService go to state and i make a method to update state accordingly

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,18 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorButton from "../error-button";
 import ErrorIndicator from "../error-indicator";
-import PeoplePage from "../people-page";
+import ErrorBoundary from "../error-boundary";
 
 import './app.css';
+import SwapiService from "../../services/swapi-service";
+import DummySwapiService from "../../services/dummy-swapi-service";
+import {SwapiServiceProvider} from "../swapi-service-contest";
+
+import {PersonDetails, PersonList, PlanetDetails, PlanetList, StarshipDetails, StarshipList,} from '../sw-components';
 
 export default class App extends Component {
 
     state = {
         showRandomPlanet: true,
         hasError: false,
+        swapiService: new DummySwapiService(),
+    };
+
+    onServiceChange = () => {
+        this.setState(({ swapiService }) => {
+            const Service = swapiService instanceof SwapiService ?
+                DummySwapiService : SwapiService;
+            return {
+                swapiService: new Service()
+            };
+        })
     };
 
     toggleRandomPlanet = () => {
@@ -36,22 +52,36 @@ export default class App extends Component {
         const planet = this.state.showRandomPlanet ? <RandomPlanet/> : null;
 
         return (
-            <div className="stardb-app">
-                <Header/>
-                { planet }
+            <ErrorBoundary>
+                <SwapiServiceProvider value={this.state.swapiService}>
+                    <div className="stardb-app">
+                        <Header onServiceChange={this.onServiceChange}/>
+                        { planet }
 
-                <div className="row mb2 button-row">
-                    <button
-                        className="toggle-planet btn btn-warning btn-lg"
-                        onClick={this.toggleRandomPlanet}>
-                        Toggle Random Planet
-                    </button>
-                    <ErrorButton/>
-                </div>
+                        <div className="row mb2 button-row">
+                            <button
+                                className="toggle-planet btn btn-warning btn-lg"
+                                onClick={this.toggleRandomPlanet}>
+                                Toggle Random Planet
+                            </button>
+                            <ErrorButton/>
+                        </div>
 
-                <PeoplePage/>
+                        <PersonDetails itemId={11} />
 
-            </div>
+                        <PlanetDetails itemId={5} />
+
+                        <StarshipDetails itemId={9} />
+
+                        <PersonList/>
+
+                        <StarshipList />
+
+                        <PlanetList />
+
+                    </div>
+                </SwapiServiceProvider>
+            </ErrorBoundary>
         );
     }
 }
