5dc5ee06adae61151a66bb6d938cc630637a0a08
Piratt14 <satdvlad@gmail.com>
2019-12-09 01:09:51 +0300

    updated HOC for the ability to change functions to receive data

--- src/components/hoc-helpers/with-data.js
+++ src/components/hoc-helpers/with-data.js
@@ -10,6 +10,16 @@ const withData = (View) => {
         };
 
         componentDidMount() {
+            this.update();
+        }
+
+        componentDidUpdate(prevProps, prevState, snapshot) {
+            if (this.props.getData !== prevProps.getData) {
+                this.update();
+            }
+        }
+
+        update() {
             this.props.getData()
                 .then((data) => {
                     this.setState({
