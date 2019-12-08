27ea4b536d6366c79a06d16bb92640f937a7ed7b
Piratt14 <satdvlad@gmail.com>
2019-12-09 00:57:25 +0300

    add new button to header for change real service and dummy service

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import './header.css';
 
-const Header = () => {
+const Header = ({ onServiceChange }) => {
     return (
         <div className="header d-flex">
             <h3>
+                {/*поменять на Star Wars DB(но так занимаемое место больше*/}
                 <a href="#">
                     Star DB
                 </a>
@@ -21,6 +22,12 @@ const Header = () => {
                     <a href="#">Starships</a>
                 </li>
             </ul>
+
+            <button
+                className="btn btn-primary btn-sm"
+                onClick={onServiceChange}>
+                Change service
+            </button>
         </div>
     );
 };
